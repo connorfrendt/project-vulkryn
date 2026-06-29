f6f14d99575583de5f964653e0415b4253eb47ce
Connor Frendt <42262048+connorfrendt@users.noreply.github.com>
2026-06-29 00:51:48 -0700

    added stats to player class

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,19 @@
 export default class Player {
     constructor(scene, x, y) {
         this.scene = scene;
+
+        // Core Stats
+        this.level = 1;
+        this.hp = 100;
+        this.maxHp = 100;
+        this.gold = 0;
+        this.abilities = [];
+
+        // Primary Stats
+        this.strength = 1;
+        this.agility = 1;
+        this.intellect = 1;
+
         this.speed = 3;
         this.sprite = scene.add.rectangle(x, y, 32, 32, 0x00ff88);
     }
